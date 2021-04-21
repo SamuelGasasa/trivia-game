813bb123f860395952ae83e5e56bd53d247339fd
orenb99 <77238204+orenb99@users.noreply.github.com>
2021-04-21 19:06:14 +0300

    started the server

--- Backend/routers/question.js
+++ Backend/routers/question.js
@@ -1,9 +1,20 @@
+const models = require("../models");
 const express = require("express");
 const question = express();
 question.use(express.json());
 
-question.get("/", (req, res) => {
+question.get("/generate", (req, res) => {
   res.send("this is the question");
 });
 
+question.post("/save", (req, res) => {
+  const { body } = req.body;
+  models.SavedQuestion.findOne({}).then((data) => console.log(data));
+});
+
+question.put("/rank?id&rank", (req, res) => {
+  const { id } = req.query;
+  const { rank } = req.query;
+  models.SavedQuestion.find({}).then((data) => console.log(data));
+});
 module.exports = question;
