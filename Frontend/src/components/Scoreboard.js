021ee8d5e0ec59f310c376c018ca81afdfe4791d
OmerBotbol <77241676+OmerBotbol@users.noreply.github.com>
2021-04-22 18:36:22 +0300

    add scoreboard component

--- Frontend/src/components/Scoreboard.js
+++ Frontend/src/components/Scoreboard.js
@@ -1,7 +1,34 @@
-import React from "react";
+import React, { useEffect } from "react";
+// import axios from "axios";
 
 function Scoreboard() {
-  return <div>Scoreboard</div>;
+  const table = [
+    { name: "Omer", score: 200, createdAt: "22-04-2020" },
+    { name: "Yuval", score: 500, createdAt: "21-04-2020" },
+    { name: "Oren", score: 300, createdAt: "23-04-2020" },
+  ];
+
+  useEffect(() => {
+    //add axios to get scoreboard
+  });
+  return (
+    <table>
+      <tr>
+        <th>name</th>
+        <th>score</th>
+        <th>date</th>
+      </tr>
+      {table.map((user) => {
+        return (
+          <tr>
+            <td>{user.name}</td>
+            <td>{user.score}</td>
+            <td>{user.createdAt}</td>
+          </tr>
+        );
+      })}
+    </table>
+  );
 }
 
 export default Scoreboard;
