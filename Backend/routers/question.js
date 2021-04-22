b047b92925b18fe2be53dbea10c847b7b7f5b1a9
orenb99 <77238204+orenb99@users.noreply.github.com>
2021-04-22 18:47:19 +0300

    finished generating

--- Backend/routers/question.js
+++ Backend/routers/question.js
@@ -14,7 +14,7 @@ const sequelize = new Sequelize(
   {
     host: "127.0.0.1",
     dialect: "mysql",
-  },
+  }
 );
 
 function shuffle(array) {
@@ -40,64 +40,13 @@ question.get("/generate", async (req, res) => {
     order: [sequelize.random()],
     limit: 4,
   };
-  if (typeNumber === 3) isGeneral && delete query.include;
-  let countries = await models.CountryGeneral.findAll(query);
-
-  if (!isGeneral)
-    answers = await Promise.all(
-      countries.map(async (country) => country["get" + table]()),
-    );
-  else answers = countries;
-
-  let filteredAnswers = answers.map((value) => {
-    return { country: value.country, field: value[field] };
-  });
-  filteredAnswers = filteredAnswers.sort((a, b) =>
-    operator ? b.field - a.field : a.field - b.field,
-  );
-  filteredAnswers.forEach((value, index) =>
-    index === 0 ? (value.right = true) : (value.right = false),
-  );
-  if (typeNumber === 2 || typeNumber === 3) {
-    question = question.replace("XXX", filteredAnswers[0].country);
-  }
-
-  if (typeNumber === 3) {
-    shuffle(filteredAnswers);
-
-    const index = Math.floor(Math.random() * 2);
-    question = question.replace("XXX", filteredAnswers[index].country);
-    question = question.replace("YYY", filteredAnswers[1 - index].country);
-  }
-
-  res.send({ question: question, answers: filteredAnswers });
-});
-
-question.get("/generate3", async (req, res) => {
-  const typeNumber = 3;
-  const tableToCommunicate = "QuestionType" + typeNumber;
-  const questionData = await models[tableToCommunicate].findOne({
-    order: [sequelize.random()],
-  });
-  let { question, field, table, operator } = questionData.toJSON();
-  const isGeneral = table === "CountriesGeneral";
-  let answers = [];
-  let query = {
-    include: [
-      {
-        model: models[table],
-        required: true,
-      },
-    ],
-    order: [sequelize.random()],
-    limit: 2,
-  };
+  typeNumber === 3 && (query.limit = 2);
   isGeneral && delete query.include;
   let countries = await models.CountryGeneral.findAll(query);
 
   if (!isGeneral)
     answers = await Promise.all(
-      countries.map(async (country) => country["get" + table]()),
+      countries.map(async (country) => country["get" + table]())
     );
   else answers = countries;
 
@@ -105,19 +54,22 @@ question.get("/generate3", async (req, res) => {
     return { country: value.country, field: value[field] };
   });
   filteredAnswers = filteredAnswers.sort((a, b) =>
-    operator ? b.field - a.field : a.field - b.field,
+    operator ? b.field - a.field : a.field - b.field
   );
   filteredAnswers.forEach((value, index) =>
-    index === 0 ? (value.right = true) : (value.right = false),
+    index === 0 ? (value.right = true) : (value.right = false)
   );
-
-  shuffle(filteredAnswers);
+  if (typeNumber === 2) {
+    question = question.replace("XXX", filteredAnswers[0].country);
+  }
 
   if (typeNumber === 3) {
+    shuffle(filteredAnswers);
     const index = Math.floor(Math.random() * 2);
     question = question.replace("XXX", filteredAnswers[index].country);
     question = question.replace("YYY", filteredAnswers[1 - index].country);
   }
+
   res.send({ question: question, answers: filteredAnswers });
 });
 
