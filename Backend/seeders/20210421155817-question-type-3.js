d7b422bb14182542bb09d2bf24751018be8a6208
OmerBotbol <77241676+OmerBotbol@users.noreply.github.com>
2021-04-22 11:42:10 +0300

    fix snake case in questions

--- Backend/seeders/20210421155817-question-type-3.js
+++ Backend/seeders/20210421155817-question-type-3.js
@@ -9,33 +9,45 @@ module.exports = {
           question: "Are there more people in X than in Y?",
           operator: true,
           field: "population",
+          created_at: new Date(),
+          updated_at: new Date(),
         },
         {
           question: "Is X larger than Y?",
           operator: true,
           field: "area_km2",
+          created_at: new Date(),
+          updated_at: new Date(),
         },
         {
           question: "Does X have a higher population density than Y?",
           operator: true,
           field: "density_pop_to_km2",
+          created_at: new Date(),
+          updated_at: new Date(),
         },
         {
           question:
             "Is the quality of life in X higher than the quality of life in Y?",
           operator: true,
           field: "quality_of_life_index",
+          created_at: new Date(),
+          updated_at: new Date(),
         },
         {
           question: "Is the crime rate of X higher than the crime rate in Y?",
           operator: true,
           field: "crime_index",
+          created_at: new Date(),
+          updated_at: new Date(),
         },
         {
           question:
             "Are restaurants in X more expensive than restaurants in Y?",
           operator: true,
           field: "restaurant_price_index",
+          created_at: new Date(),
+          updated_at: new Date(),
         },
       ],
       {}
