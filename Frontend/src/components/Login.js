c123705ed60eb2574b44c2aeef396add15da0869
OmerBotbol <77241676+OmerBotbol@users.noreply.github.com>
2021-04-22 18:00:03 +0300

    finish log in route
    need to add axios

--- Frontend/src/components/Login.js
+++ Frontend/src/components/Login.js
@@ -1,28 +1,29 @@
 import React, { useState } from "react";
+import { Link } from "react-router-dom";
 // import axios from "axios";
 import "../styles/Login.css";
 
 function Login() {
   const [name, setName] = useState("");
 
-  const handleSubmit = () => {};
+  const handleSubmit = () => {
+    //need to add axios to leader board
+  };
 
   return (
     <div className="login-page">
       <h1 id="form-headline">LOG-IN</h1>
-      <form
-        onSubmit={() => {
-          handleSubmit();
-        }}
-      >
-        <input
-          value={name}
-          onChange={(event) => setName(event.target.value)}
-          id="name"
-          placeholder="Enter your name"
-        />
-        <input id="submit" type="submit" value="submit" />
-      </form>
+      <input
+        value={name}
+        onChange={(event) => setName(event.target.value)}
+        id="name"
+        placeholder="Enter your name"
+      />
+      <Link exact="true" to="/question">
+        <button id="submit" onClick={() => handleSubmit()}>
+          Log-in
+        </button>
+      </Link>
     </div>
   );
 }
