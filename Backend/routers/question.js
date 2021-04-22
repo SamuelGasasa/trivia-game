4b201e9675bda097588366ef22c2a4abd854bd0f
SamuelGasasa <77382869+SamuelGasasa@users.noreply.github.com>
2021-04-22 13:44:05 +0300

    created view

--- Backend/routers/question.js
+++ Backend/routers/question.js
@@ -1,9 +1,21 @@
 const models = require("../models");
 const { Op } = require("sequelize");
-const sequelize = require("Sequelize");
+const Sequelize = require("sequelize");
 const express = require("express");
+const { QueryTypes } = require("sequelize");
 const question = express();
+require("dotenv").config();
 question.use(express.json());
+
+const sequelize = new Sequelize(
+  process.env.database,
+  "root",
+  process.env.password,
+  {
+    host: "127.0.0.1",
+    dialect: "mysql",
+  },
+);
 
 question.get("/generate", async (req, res) => {
   // const typeNumber = Math.floor(Math.random() * 3) + 1;
@@ -14,11 +26,21 @@ question.get("/generate", async (req, res) => {
   console.log(field);
   switch (typeNumber) {
     case 1:
-      const possibleAnswers = await models.CountryGeneral.findAll({
-        where: field,
-        limit: 4,
-      });
-      console.log(possibleAnswers);
+      sequelize
+        .query("SELECT population FROM `example` LIMIT 4", {
+          type: sequelize.QueryTypes.SELECT,
+        })
+        .then((res) => console.log(res));
+      // const questions = await sequelize.query(
+      //   "SELECT population FROM example LIMIT 4",
+      //   { type: QueryTypes.SELECT },
+      // );
+      // console.log(questions);
+      // const possibleAnswers = await models.CountryGeneral.findAll({
+      //   where: field,
+      //   limit: 4,
+      // });
+      // console.log(possibleAnswers);
       break;
 
     default:
