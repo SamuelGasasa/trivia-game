6b12331acb4c228ae0a150618ddfb4b16a150894
OmerBotbol <77241676+OmerBotbol@users.noreply.github.com>
2021-04-23 22:08:36 +0300

    add TempRightAnswer table

---
+++ Backend/migrations/20210423165221-create-temp-right-answer.js
@@ -0,0 +1,27 @@
+"use strict";
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable("temp_right_answers", {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      answer: {
+        type: Sequelize.STRING,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable("temp_right_answers");
+  },
+};
