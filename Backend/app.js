de4d26c35510ac7b40d8ed87b2efb5d23c86b5c1
orenb99 <77238204+orenb99@users.noreply.github.com>
2021-04-28 15:18:50 +0300

    adding user route

--- Backend/app.js
+++ Backend/app.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const question = require("./routers/question");
 const scoreboard = require("./routers/scoreboard");
+const users = require("./routers/users");
+
 const app = express();
 
 app.use("/question", question);
 app.use("/scoreboard", scoreboard);
-
+app.use("/users", users);
 module.exports = app;
--- Backend/routers/users.js
+++ Backend/routers/users.js
@@ -1,6 +1,14 @@
-const express = require("express");
+require("dotenv").config();
+const models = require("../models");
+const { Op } = require("sequelize");
 const Sequelize = require("sequelize");
-
+const express = require("express");
 const users = express();
-
 users.use(express.json());
+
+users.get("/test", async (req, res) => {
+  const player = await models.Score.findOne({});
+  res.send(player);
+});
+
+module.exports = users;
