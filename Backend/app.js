bd4549707fd4da13fb04e7505134eff41e9e7084
orenb99 <77238204+orenb99@users.noreply.github.com>
2021-05-10 13:59:18 +0300

    connecting the react with the backend

--- Backend/app.js
+++ Backend/app.js
@@ -1,17 +1,19 @@
 const express = require("express");
+const path = require("path");
 const information = require("./routers/information");
 const question = require("./routers/question");
 const scoreboard = require("./routers/scoreboard");
 const users = require("./routers/users");
 
 const app = express();
+app.use(express.static(path.join(__dirname, "../build")));
 
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "../build", "index.html"));
+});
 app.use("/question", question);
 app.use("/scoreboard", scoreboard);
 app.use("/users", users);
 app.use("/information", information);
 
-app.get("/", (req, res) => {
-  res.send("hello");
-});
 module.exports = app;
